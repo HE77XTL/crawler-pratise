66aae4f9c043e2e4632209e601afc5c6275da070
何东升 <he77505357@163.com>
2022-01-15 16:37:32 +0800

    feat(web): 增加简单的前端搜索展示页

---
+++ src/main/java/com/github/hextl/entity/Total.java
@@ -0,0 +1,13 @@
+package com.github.hextl.entity;
+
+public class Total {
+    Integer total;
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+}
--- src/main/java/com/github/hextl/service/ElasticSearch.java
+++ src/main/java/com/github/hextl/service/ElasticSearch.java
@@ -2,6 +2,7 @@ package com.github.hextl.service;
 
 
 import com.github.hextl.entity.News;
+import com.github.hextl.entity.Total;
 import org.apache.http.HttpHost;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -39,13 +40,16 @@ public class ElasticSearch {
         try (RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost("101.43.38.193", 9200, "http")))) {
-            for (int i = 0; i < 10; i++) {
-                BulkRequest bulkRequest = new BulkRequest();
-                //索引请求的对象
-                IndexRequest request = new IndexRequest("news");
-                //指定变量到属性的映射
 
-                List<News> list = elasticSearch.getNewsFromDB();
+            BulkRequest bulkRequest = new BulkRequest();
+            //索引请求的对象
+            IndexRequest request = new IndexRequest("news");
+            //指定变量到属性的映射
+            Integer newsTotal = elasticSearch.getNewsTotal();
+            Integer page = 0;
+
+            while (newsTotal > 0) {
+                List<News> list = elasticSearch.getNewsFromDB(page);
                 for (News news : list
                 ) {
                     Map<String, Object> data = new HashMap<>();
@@ -59,17 +63,28 @@ public class ElasticSearch {
                     bulkRequest.add(request);
                 }
                 client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                page ++;
+                newsTotal -= 10;
             }
         }
     }
 
 
 
-    public List<News> getNewsFromDB() {
+    public List<News> getNewsFromDB(Integer page) {
         List<News> list;
         try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-            list = sqlSession.selectList("selectNewFromNews");
+            list = sqlSession.selectList("selectNewFromNews", page);
         }
         return list;
+    }
+
+    public Integer getNewsTotal() {
+        Integer newsTotal = 0;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            List<Total> total = sqlSession.selectList("selectNewCount");
+            newsTotal = total.get(0).getTotal();
+        }
+        return  newsTotal;
     }
 }
